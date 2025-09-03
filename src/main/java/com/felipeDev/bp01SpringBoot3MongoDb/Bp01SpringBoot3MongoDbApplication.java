2737fe9667da32b0c03a1d3be7a562c9cd5d2f7e
FelipeeFerreiraa <felipe333ferreira333@gmail.com>
2025-09-02 21:11:18 -0300

    start --> 02/09/25 21:10

---
+++ src/main/java/com/felipeDev/bp01SpringBoot3MongoDb/Bp01SpringBoot3MongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.felipeDev.bp01SpringBoot3MongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bp01SpringBoot3MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bp01SpringBoot3MongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipeDev/bp01SpringBoot3MongoDb/Bp01SpringBoot3MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipeDev.bp01SpringBoot3MongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bp01SpringBoot3MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
