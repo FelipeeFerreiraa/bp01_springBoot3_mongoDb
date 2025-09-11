fb4156aa6968eff218ea6fe4307753fb55e2cefa
FelipeeFerreiraa <felipe333ferreira333@gmail.com>
2025-09-11 20:43:15 -0300

    criando entidade Post co User aninhado, acessando pelo MongoCompass --> 11/09/25 20:40

--- src/main/java/com/felipeDev/bp01SpringBoot3MongoDb/config/Instantiation.java
+++ src/main/java/com/felipeDev/bp01SpringBoot3MongoDb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.felipeDev.bp01SpringBoot3MongoDb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.felipeDev.bp01SpringBoot3MongoDb.domain.Post;
 import com.felipeDev.bp01SpringBoot3MongoDb.domain.User;
+import com.felipeDev.bp01SpringBoot3MongoDb.repository.PostRepository;
 import com.felipeDev.bp01SpringBoot3MongoDb.repository.UserRepository;
 
 @Configuration
@@ -15,17 +19,33 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
+		SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+		fmt.setTimeZone(TimeZone.getTimeZone("GMT")); // HORARIO DE LONDRES
+
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		Post post1 = new Post(null, "Livro entendendo algoritmos",
+				"Livro para aprendizado de algoritmos básicos para a computação, usando python.....",
+				fmt.parse("21/03/2025"), maria);
+
+		Post post2 = new Post(null, "Livro Senhores da guerra",
+				"Livro sobre os principais individuos da segunda guerra mundia, sendo um britanico, um americano um russo e outro alemão",
+				fmt.parse("29/03/2025"), maria);
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/felipeDev/bp01SpringBoot3MongoDb/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.felipeDev.bp01SpringBoot3MongoDb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipeDev.bp01SpringBoot3MongoDb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
